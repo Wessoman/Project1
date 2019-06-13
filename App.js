5e17f6dd0008f46f5196276a60b71b3cf1cc26f6
Antony Hernandez <wessoman@hotmail.com>
2019-06-13 13:50:08 -0500

    Added Firebase access to HTML and Java

---
+++ App.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCcgoGoTHLroUvcnjjfDgPy1sHzyWPAFX8",
+    authDomain: "musicproject1-e582b.firebaseapp.com",
+    databaseURL: "https://musicproject1-e582b.firebaseio.com",
+    projectId: "musicproject1-e582b",
+    storageBucket: "musicproject1-e582b.appspot.com",
+    messagingSenderId: "1050934926486",
+    appId: "1:1050934926486:web:e9fcdda165569b47"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
