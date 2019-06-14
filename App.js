cabe9fdcb3f682813fcc9532090a46db4587c509
Anderson <andersonswaimboyd@gmail.com>
2019-06-14 09:54:54 -0500

    added google map

--- App.js
+++ App.js
@@ -10,3 +10,12 @@ var firebaseConfig = {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+  function initMap (){
+    var austin = {
+      lat: 30.2672,
+      lng: -97.7431
+    };
+    var map = new google.maps.Map(document.getElementById("map"), {zoom: 10, center: austin});
+    var marker = new google.maps.Marker({position: austin, map: map});
+  }
